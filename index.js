d7e92742bc9f5ba157db845bda837af51b894ca2
AliKazim7 <aghaalikazim50@gmail.com>
2020-02-29 14:08:13 +0500

    new homme screen

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ import App from './App';
 import Main from './src/Main'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
